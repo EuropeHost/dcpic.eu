7b833d9d243259f5eeb1b6cb06bafb7da3a3940c
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-05-17 21:00:31 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'discord_id',
+        'avatar',
+    ];
+
+    protected $hidden = [
+        'remember_token',
+    ];
+}
