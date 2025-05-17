bcd87733a565b38a3f91248e996fe77caa48484e
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-05-17 20:59:46 +0200

    Create web.php

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::get('/', function () {
+    return 'Welcome to dcpics.eu!';
+});
+
+Route::get('/auth/redirect', [AuthController::class, 'redirectToDiscord'])->name('login');
+Route::get('/auth/callback', [AuthController::class, 'handleDiscordCallback']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
+        return 'Logged in as ' . auth()->user()->name;
+    })->name('dashboard');
+});
