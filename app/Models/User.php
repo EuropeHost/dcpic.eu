918406aa859f95fb7f13ca8badaab337cbfe3ccb
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-05-18 15:25:41 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Image;
 
 class User extends Authenticatable
 {
@@ -19,4 +20,46 @@ class User extends Authenticatable
     protected $hidden = [
         'remember_token',
     ];
+	
+	public function images()
+	{
+	    return $this->hasMany(Image::class);
+	}
+	
+	public function getStorageUsedAttribute()
+	{
+	    return $this->images()->sum('size');
+	}
+	
+	public function getStorageUsedMBAttribute()
+	{
+	    return round($this->storage_used / 1024 / 1024, 2);
+	}
+	
+	/*
+	public function getStorageLimitMBAttribute()
+	{
+	    return 250;
+	}
+	
+	public function getStoragePercentageAttribute()
+	{
+	    return min(100, round(($this->storage_used / (250 * 1024 * 1024)) * 100, 2));
+	}
+	*/
+	
+	public function getStorageLimitMBAttribute()
+	{
+	    return 25;
+	}
+	
+	public function getStoragePercentageAttribute()
+	{
+	    return min(100, round(($this->storage_used / (25 * 1024 * 1024)) * 100, 2));
+	}
+	
+	public function storage_limit()
+	{
+		return $this->getStorageLimitMBAttribute;
+	}
 }
