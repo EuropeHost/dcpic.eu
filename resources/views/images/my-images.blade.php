a8d16a37a64e92fc1c4a516027fe53efedb8c371
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-05-23 00:02:59 +0200

    Update my-images.blade.php

--- resources/views/images/my-images.blade.php
+++ resources/views/images/my-images.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('main')
-    <h1 class="text-2xl font-semibold mb-6">My Images</h1>
+    <h1 class="text-2xl font-semibold mb-6">{{ __('content.my_images') }}</h1>
 
     @include('images._list', ['images' => $images])
 @endsection
