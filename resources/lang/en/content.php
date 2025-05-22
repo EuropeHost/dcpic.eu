fbe349ff80ac0c49de5a582f0982889b4f727ccc
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-05-22 23:11:43 +0200

    Update content.php

--- resources/lang/en/content.php
+++ resources/lang/en/content.php
@@ -1,1 +1,12 @@
+<?php
 
+return [
+    'home_title' => 'Welcome to dcpic.eu',
+    'home_subtitle' => 'A Discord-based image hosting platform',
+    'login_with_discord' => 'Log in with Discord',
+    'storage_used' => 'Storage Used',
+    'upload_image' => 'Upload New Image',
+    'upload' => 'Upload',
+    'latest_upload' => 'Latest Upload',
+    'see_all' => 'See All Images',
+];
