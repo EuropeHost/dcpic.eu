2593731ef3a4b1c0f6bfeed793ebfee9bdf74f27
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-05-24 21:43:35 +0200

    Create legal.php

---
+++ resources/lang/de/legal.php
@@ -0,0 +1,93 @@
+<?php
+
+return [
+    'imprint' => [
+        'title' => 'Impressum',
+        'content' => <<<EOT
+### Verantwortliche Stelle
+
+Dieser Dienst wird betrieben von **EuropeHost.eu**, einer Marke von **XPSystems.eu**, geführt von **Fabian Ternis**.
+
+Verantwortlich gemäß § 5 TMG:
+
+- **Fabian Ternis**, XPSystems.eu  
+- E-Mail: [f.ternis@xpsystems.eu](mailto:f.ternis@xpsystems.eu)  
+- Website: [xpsystems.eu](https://xpsystems.eu)
+
+### Urheberrecht
+
+Alle hochgeladenen Bilder bleiben Eigentum ihrer Urheber.  
+DCPic.eu beansprucht keine Rechte an Inhalten.  
+Mit dem Hochladen erteilen Sie eine nicht-exklusive Lizenz zur Speicherung und Auslieferung.
+
+Urheberrechtsverstöße führen zur sofortigen Löschung und möglichen Sperrung.
+
+### Kontakt
+
+Rechtliche Anfragen bitte an: [f.ternis@xpsystems.eu](mailto:f.ternis@xpsystems.eu)
+EOT
+    ],
+
+    'privacy' => [
+        'title' => 'Datenschutzerklärung',
+        'content' => <<<EOT
+### Welche Daten wir erfassen
+
+Wir speichern nur die notwendigsten Daten:
+
+- Ihre Discord-ID und Benutzername
+- Metadaten Ihrer hochgeladenen Bilder (Dateiname, Größe, Format)
+- Zeitstempel und Speicherverbrauch
+
+### Bildzugriff
+
+Ihre Bilder sind **standardmäßig privat**.  
+Nur Personen mit Direktlink können sie aufrufen.
+
+### Kein Tracking
+
+Diese Seite verwendet **keine Cookies**, keine Werbung und kein externes Tracking.
+
+### Datenlöschung
+
+Sie können jederzeit die Löschung Ihrer Daten anfordern.  
+Senden Sie eine E-Mail von Ihrem Discord-Konto an [f.ternis@xpsystems.eu](mailto:f.ternis@xpsystems.eu).
+
+### Hosting & DSGVO
+
+Alle Daten werden auf Servern innerhalb der EU gespeichert und unterliegen der **DSGVO**.
+EOT
+    ],
+
+    'terms' => [
+        'title' => 'Nutzungsbedingungen',
+        'content' => <<<EOT
+### Nutzungsregeln
+
+- Kein Upload illegaler, schädlicher, hasserfüllter oder pornografischer Inhalte.
+- Inhalte müssen deutschem und EU-Recht entsprechen.
+- Sie tragen die volle Verantwortung für hochgeladene Inhalte.
+
+### Urheberrecht
+
+Sie behalten das volle Urheberrecht.  
+Wir benötigen lediglich eine Lizenz zur Speicherung und Anzeige.
+
+### Speichergrenzen
+
+Jeder Benutzer hat maximal 25MB Speicher.  
+Versuche zur Umgehung können zur Sperrung führen.
+
+### Verfügbarkeit
+
+Dieser Dienst ist **experimentell und kostenlos**.  
+Es besteht kein Anspruch auf Verfügbarkeit oder Erreichbarkeit.  
+Der Dienst kann jederzeit verändert oder eingestellt werden.
+
+### Änderungen
+
+Mit Nutzung akzeptieren Sie diese Bedingungen.  
+Diese können jederzeit angepasst werden.
+EOT
+    ],
+];
