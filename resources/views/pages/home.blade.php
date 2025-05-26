0db2a7520b2425c1ba0db8954261b1f92f2450f4
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-05-26 21:28:01 +0200

    Update home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,15 +1,18 @@
 @extends('layouts.main')
 
 @section('main')
-    <div class="text-center py-12">
+    <div class="text-center py-12 home">
         <h1 class="text-4xl font-bold">{{ __('content.home_title') }}</h1>
         <p class="text-lg mt-4 text-gray-600">{{ __('content.home_subtitle') }}</p>
 
         @guest
-            <a href="{{ route('login') }}"
+            <!--a href="{{ route('login') }}"
                class="mt-6 inline-block bg-sky-600 text-white px-6 py-3 rounded hover:bg-sky-700 transition text-lg">
                 <i class="bi bi-discord"></i> {{ __('content.login_with_discord') }}
-            </a>
+            </a-->
+			<a href="{{ route('login') }}" class="discord-login-btn mt-6">
+			    <i class="bi bi-discord"></i> {{ __('content.login_with_discord') }}
+			</a>
         @endguest
 
         <div class="mt-10 max-w-md mx-auto bg-white border shadow rounded p-6 text-left">
