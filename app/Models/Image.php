ca7ae9dba3e66ab361c72b9c9b56dafd03b904ee
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-05-26 21:57:17 +0200

    Update Image.php

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     protected $fillable = [
-        'user_id', 'filename', 'original_name', 'mime', 'size',
+        'user_id', 'filename', 'original_name', 'mime', 'size', 'is_public',
     ];
 
     public function user()
