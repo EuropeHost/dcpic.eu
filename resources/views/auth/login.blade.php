c5e1072e0d7066debe471e9b799e607f26d47b15
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-05-26 21:28:19 +0200

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('main')
-    <div class="text-center py-20">
+    <div class="text-center py-20 auth">
 		<button class="discord-login-btn mb-4" type="button" href="{{ route('login') }}">
     	    <i class="bi bi-discord" aria-hidden="true"></i>
     	    {{ __('content.login_with_discord') }}
