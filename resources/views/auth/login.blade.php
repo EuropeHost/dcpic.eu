629ecd1a727b481edbf6d5d646ed11c4741b54c3
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-05-26 21:08:10 +0200

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,8 +2,9 @@
 
 @section('main')
     <div class="text-center py-20">
-        <a href="{{ route('login') }}" class="bg-indigo-600 text-white px-4 py-2 rounded shadow hover:bg-indigo-700">
-            <i class="bi bi-discord"></i> {{ __('content.login_with_discord') }}
-        </a>
+		<button class="discord-login-btn mb-4" type="button" href="{{ route('login') }}">
+    	    <i class="bi bi-discord" aria-hidden="true"></i>
+    	    {{ __('content.login_with_discord') }}
+    	</button>
     </div>
 @endsection
