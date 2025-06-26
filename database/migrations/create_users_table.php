cb0c9668f288f5c4b86fb132a2c0367f4c7b306e
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-06-26 16:24:47 +0200

    Update create_users_table.php

--- database/migrations/create_users_table.php
+++ database/migrations/create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up()
 	{
 	    Schema::create('users', function (Blueprint $table) {
-	        $table->uuid('id')->primary();
+	        $table->id();
 	        $table->string('name');
 	        $table->string('email')->nullable()->unique();
 	        $table->string('discord_id')->unique();
