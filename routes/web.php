7523d498694c342823e5c9dfbff4d6272fdaec0d
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-07-07 21:01:29 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ImageController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\LegalController;
+use App\Http\Controllers\AdminController;
 
 Route::post('/locale', function (Request $request) {
     session(['locale' => $request->locale]);
@@ -26,6 +27,12 @@ Route::middleware('auth')->group(function () {
     Route::post('/images', [ImageController::class, 'store'])->name('images.store');
     Route::delete('/images/{image}', [ImageController::class, 'destroy'])->name('images.destroy');
 	Route::patch('/images/{image}/visibility', [ImageController::class, 'toggleVisibility'])->name('images.toggleVisibility');
+	
+	Route::middleware('admin')->prefix('admin')->name('admin.')->group(function () {
+        Route::get('/dashboard', [AdminController::class, 'index'])->name('dashboard');
+        Route::get('/', [AdminController::class, 'index'])->name('overview');
+		Route::get('/users/{user}', [AdminController::class, 'showUser'])->name('users.show');
+    });
 });
 
 Route::get('/recent-uploads', [ImageController::class, 'recentUploads'])->name('images.recent');
@@ -41,3 +48,8 @@ Route::get('/video/{image}', [ImageController::class, 'show']);
 Route::get('/media/{image}', [ImageController::class, 'show']);
 
 Route::get('/legal/{section}', [LegalController::class, 'show'])->name('legal.show');
+
+Route::post('/announcement/dismiss/{id}', function ($id) {
+    session()->put("announcement_dismissed_{$id}", true);
+    return back();
+})->name('announcement.dismiss');
