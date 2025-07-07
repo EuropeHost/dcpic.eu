43cfae0614b80111f73ec3f5d2e0554a717b59aa
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-07-07 12:59:15 +0200

    Update PageController.php

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -8,26 +8,26 @@ use App\Models\User;
 
 class PageController extends Controller
 {
-	public function home()
-	{
-	    $totalUsed = Image::sum('size');
-	    $totalUsers = \App\Models\User::count();
-	    $avgPerUser = $totalUsers > 0 ? $totalUsed / $totalUsers : 0;
-	
-	    $totalLimit = env('STORAGE_LIMIT') * 1024 * 1024 * 1024; // STORAGE_LIMIT GiB in bytes
-	    $storagePercentage = min(100, ($totalUsed / $totalLimit) * 100);
-	
-	    return view('pages.home', [
-	        'totalUsed' => $totalUsed,
-	        'avgPerUser' => $avgPerUser,
-	        'totalLimit' => $totalLimit,
-	        'storagePercentage' => $storagePercentage,
-	    ]);
-	}
+    public function home()
+    {
+        $totalUsed = Image::sum('size');
+        $totalUsers = User::count();
+        $avgPerUser = $totalUsers > 0 ? $totalUsed / $totalUsers : 0;
+
+        $totalLimit = env('STORAGE_LIMIT') * 1024 * 1024 * 1024; // STORAGE_LIMIT GiB in bytes
+        $storagePercentage = min(100, ($totalUsed / $totalLimit) * 100);
+
+        return view('pages.home', [
+            'totalUsed' => $totalUsed,
+            'avgPerUser' => $avgPerUser,
+            'totalLimit' => $totalLimit,
+            'storagePercentage' => $storagePercentage,
+        ]);
+    }
 
     public function dashboard()
     {
-        $latestImage = auth()->user()->images()->latest()->first();
-        return view('pages.dashboard', compact('latestImage'));
+        $latestImages = auth()->user()->images()->latest()->take(5)->get();
+        return view('pages.dashboard', compact('latestImages'));
     }
 }
