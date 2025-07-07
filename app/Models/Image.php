06738374f00537859732323081f2a8200939f006
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-07-07 10:20:13 +0200

    Update Image.php

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -9,6 +9,17 @@ class Image extends Model
     protected $fillable = [
         'user_id', 'filename', 'original_name', 'mime', 'size', 'is_public',
     ];
+	
+	protected $keyType = 'string';
+	public $incrementing = false;
+	
+	protected static function boot()
+	{
+	    parent::boot();
+	    static::creating(function ($model) {
+	        $model->id = (string) Str::uuid();
+	    });
+	}
 
     public function user()
     {
