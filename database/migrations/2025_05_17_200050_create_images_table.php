b70fd36ac061774af8776b6e19daf47d6f761c86
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-07-07 10:18:58 +0200

    Update 2025_05_17_200050_create_images_table.php

--- database/migrations/2025_05_17_200050_create_images_table.php
+++ database/migrations/2025_05_17_200050_create_images_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
 	{
 	    Schema::create('images', function (Blueprint $table) {
-	        $table->id();
-	        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+	        $table->uuid('id')->primary();
+	        $table->foreignUuid('user_id')->constrained()->onDelete('cascade');
 	        $table->string('filename')->unique();
 	        $table->string('original_name');
 	        $table->string('mime');
