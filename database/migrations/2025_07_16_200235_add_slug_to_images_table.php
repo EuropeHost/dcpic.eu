c26aaf55fb97fb2ca20e265dcc1c634d5e8874a6
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-07-16 22:12:00 +0200

    Update 2025_07_16_200235_add_slug_to_images_table.php

--- database/migrations/2025_07_16_200235_add_slug_to_images_table.php
+++ database/migrations/2025_07_16_200235_add_slug_to_images_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+use App\Models\Image;
 
 return new class extends Migration
 {
@@ -15,6 +17,13 @@ return new class extends Migration
     {
         Schema::table('images', function (Blueprint $table) {
             $table->string('slug', 7)->unique()->nullable()->after('id');
+        });
+		
+        Image::cursor()->each(function (Image $image) {
+            if (empty($image->slug)) {
+                $image->slug = Str::random(7);
+                $image->saveQuietly(); // avoid triggering events
+            }
         });
     }
 
