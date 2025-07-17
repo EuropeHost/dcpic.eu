80dc4181272ca4e1cb69858ba1b3b5f2ebac6bf1
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-07-17 17:31:11 +0200

    Update Image.php

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -2,36 +2,51 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
 class Image extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
-        'user_id', 'filename', 'slug', 'original_name', 'mime', 'size', 'is_public',
+        'user_id', 'type', 'filename', 'original_name', 'mime', 'size', 'is_public', 'slug',
     ];
-	
-	protected $keyType = 'string';
-	public $incrementing = false;
-	
-	protected static function boot()
-	{
-	    parent::boot();
-	
-	    static::creating(function ($model) {
-	        if (empty($model->id)) {
-	            $model->id = (string) Str::uuid();
-	        }
-	    });
-	}
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = (string) Str::uuid();
+            }
+
+            if (empty($model->slug)) {
+                $model->slug = self::generateUniqueSlug();
+            }
+        });
+    }
+
+    protected static function generateUniqueSlug()
+    {
+        do {
+            $slug = Str::random(7);
+        } while (self::where('slug', $slug)->exists());
+        return $slug;
+    }
 
     public function user()
     {
         return $this->belongsTo(User::class);
-    }
-	
-    public function getRouteKeyName()
-    {
-        return 'slug';
     }
 }
