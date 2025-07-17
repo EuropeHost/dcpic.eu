e4d0e88da201f4b589189f13c4bb16a4fef144ce
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-07-17 17:31:38 +0200

    Create Link.php

---
+++ app/Models/Link.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Link extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'original_url', 'slug', 'visits',
+    ];
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = (string) Str::uuid();
+            }
+
+            if (empty($model->slug)) {
+                $model->slug = self::generateUniqueSlug();
+            }
+        });
+    }
+
+    protected static function generateUniqueSlug()
+    {
+        do {
+            $slug = Str::random(7);
+        } while (self::where('slug', $slug)->exists());
+        return $slug;
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
