d90c29b05f6e4e222451780df0a762717bc736f4
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-07-17 17:22:27 +0200

    Create 2025_07_16_201230_create_links_table.php

---
+++ database/migrations/2025_07_16_201230_create_links_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('user_id')->constrained()->onDelete('cascade');
+            $table->string('original_url');
+            $table->string('slug', 7)->unique();
+            $table->integer('visits')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+};
