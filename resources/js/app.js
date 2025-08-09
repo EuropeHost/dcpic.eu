bf8302c4ce73e2df2a2d0eba772c98529a83707f
Michaelninder <fabianeternis@gmail.com>
2025-08-09 13:26:12 +0200

    test: move custom css files to resources/css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+
+import '../css/animations.css';
+import '../css/discord-login-btn.css';
+import '../css/feature-card.css';
+import '../css/preloader.css';
+import '../css/scroll-down-btn.css';
+import '../css/stats.css';
